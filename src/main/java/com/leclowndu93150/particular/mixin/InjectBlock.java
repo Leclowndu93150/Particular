7ba581c1a5a171835545a782dfb0c69f8600042a
John Hoffman <hoffmandavidjohn@gmail.com>
2026-07-03 13:27:13 -0600

    Fix cave dust blacklist biome matching

--- src/main/java/com/leclowndu93150/particular/mixin/InjectBlock.java
+++ src/main/java/com/leclowndu93150/particular/mixin/InjectBlock.java
@@ -27,7 +27,7 @@ public class InjectBlock
 	private static boolean isValidBiome(Holder<Biome> biome)
 	{
 		var key = biome.unwrapKey();
-		return key.map(biomeRegistryKey -> !ParticularConfig.COMMON.caveDustExcludeBiomes.get().contains(biomeRegistryKey.location())).orElse(true);
+		return key.map(biomeRegistryKey -> !ParticularConfig.getCaveDustExcludeBiomes().contains(biomeRegistryKey.location())).orElse(true);
 	}
 
 	@Inject(at = @At("TAIL"), method = "animateTick")
